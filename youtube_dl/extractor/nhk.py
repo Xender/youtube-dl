190b2bfd3f244e580be64b24340f6331fc99905d
Aleksander Nitecki <ixendr@itogi.re>
2016-08-24 21:34:03 +0200

    [nhk] Add test case (will become outdated anyway).

--- youtube_dl/extractor/nhk.py
+++ youtube_dl/extractor/nhk.py
@@ -5,7 +5,17 @@ from .common import InfoExtractor
 
 class NhkVodIE(InfoExtractor):
     _VALID_URL = r'http://www3.nhk.or.jp/nhkworld/en/vod/(?P<id>.+)\.html'
-    # _TESTS = []  # TODO videos I've found are all available only for a limited period of time. That automatically makes such test cases obsolete. :/
+    _TESTS = [{
+        'url': 'http://www3.nhk.or.jp/nhkworld/en/vod/tokyofashion/20160815.html',
+        'info_dict': {
+            'id': 'A1bnNiNTE6nY3jLllS-BIISfcC_PpvF5',
+            'ext': 'flv',
+            'title': '[nhkworld]VOD;2009-251-2016;TOKYO FASHION EXPRESS;The Kimono as Global Fashion;en',
+        },
+        'params': {
+            'skip_download': True  # Videos available only for a limited period of time.
+        },
+    }]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
